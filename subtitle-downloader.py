a4ad2d4159d59069def79fb616df197e9fcc8e02
Manoj M J <manojmj92@gmail.com>
2014-02-11 23:34:13 +0530

    Update subtitle-downloader.py

--- subtitle-downloader.py
+++ subtitle-downloader.py
@@ -15,7 +15,7 @@ import sys
 def get_hash(name):
         readsize = 64 * 1024
         with open(name, 'rb') as f:
-            size = os.path.getsize(name)
+            
             data = f.read(readsize)
             f.seek(-readsize, os.SEEK_END)
             data += f.read(readsize)
