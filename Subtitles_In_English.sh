79a82a53128f1f9362f0ce597b9b169c19658d9f
rahil sharma <liharsharma91092@gmail.com>
2014-05-25 20:20:20 -0700

    updated path

--- Subtitles_In_English.sh
+++ Subtitles_In_English.sh
@@ -15,3 +15,4 @@ IFS=$IFS_BAK
 
 ##to do::#give a check here to see if the files coming are .mp4........videoformat files only otherwise exit.....
 #to do::also we can give languagae as second parameter
+#path to save in ~/.gnome2/nautilus-scripts
