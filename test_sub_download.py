2916f90929fd39e943cdb816534e917de5f579e0
Nick R <niroyb+github@gmail.com>
2015-11-01 13:54:52 -0800

    Test the RNG consistency

--- test_sub_download.py
+++ test_sub_download.py
@@ -9,12 +9,12 @@ import unittest
 import subtitle_downloader as subdl
 
 MAJOR_VERSION = sys.version_info[0]
+
 class TestSubDownloader(unittest.TestCase):
 
     '''Unit tests for subtitle_downloader methods'''
-    def test_print_version(self):
+    def test_major_version(self):
         '''If we are not python 2 or 3, update travis ci and this test.'''
-        print(sys.version_info)
         self.assertTrue(MAJOR_VERSION in [2, 3])
 
     def test_get_subtitles(self):
@@ -32,15 +32,17 @@ class TestSubDownloader(unittest.TestCase):
         file_descriptor, tmp_path = tempfile.mkstemp()
         os.close(file_descriptor)
         with open(tmp_path, 'wb') as tmp_file:
-            for _ in range(100):
-                plaintext = str(random.random()) * random.randint(1, 1000)
-                if MAJOR_VERSION == 2:
-                    tmp_file.write(plaintext)
-                elif MAJOR_VERSION == 3:
-                    tmp_file.write(bytes(plaintext, 'UTF-8'))
+            rand_data = [random.random() for _ in range(10000)]
+            plaintext = "".join(map(str, rand_data))
+            if MAJOR_VERSION == 2:
+                tmp_file.write(plaintext)
+            elif MAJOR_VERSION == 3:
+                tmp_file.write(bytes(plaintext, 'UTF-8'))
         hashed = subdl.get_hash(tmp_path)
         os.remove(tmp_path)
-        self.assertEqual("fe2cbd27befbf4c08cd611df049d0267", hashed)
+        rand_str = "{:16.14f}".format(rand_data[0])
+        self.assertEqual("0.61775285695147", rand_str, "Random generator is not trustworthy.")
+        self.assertEqual("aa35ffc8f7c7f1f8ae687e08c40c50c8", hashed)
 
     def test_is_movie_extension(self):
         '''Test the is_movie_file_extention function.'''
