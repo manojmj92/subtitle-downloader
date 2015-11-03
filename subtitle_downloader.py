8ac20ca3ed62211699e9fd97f723267dff597152
Nick R <niroyb+github@gmail.com>
2015-11-02 23:27:56 -0800

    pep8 formatting

--- subtitle_downloader.py
+++ subtitle_downloader.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+'''Subtitle downloader utility using thesubdb'''
 # -------------------------------------------------------------------------------
 # Name      : subtitle downloader
 # Purpose   : One step subtitle download
@@ -24,17 +25,20 @@ if PY_VERSION == 3:
 
 
 def get_hash(file_path):
-    '''Returns the hash of a file as expected by subDB: http://thesubdb.com/api/'''
+    '''Returns the hash of a file as expected by subDB: http://thesubdb.com/api/
+    We return the md5 of the concatenated first and last 64kb of data'''
     read_size = 64 * 1024
-    with open(file_path, 'rb') as f:
-        data = f.read(read_size)
-        f.seek(-read_size, os.SEEK_END)
-        data += f.read(read_size)
+    with open(file_path, 'rb') as file_obj:
+        data = file_obj.read(read_size)
+        file_obj.seek(-read_size, os.SEEK_END)
+        data += file_obj.read(read_size)
     return hashlib.md5(data).hexdigest()
 
+
 def get_subtitles(file_hash):
-    headers = {'User-Agent': 'SubDB/1.0 (subtitle_downloader/1.0; http://github.com/manojmj92/subtitle_downloader)'}
-    url = "http://api.thesubdb.com/?action=download&hash=" + file_hash + "&language=en"
+    '''Return the subtitles text for the given movie file hash.'''
+    headers = {'User-Agent':'SubDB/1.0 (subtitle_downloader/1.0; http://github.com/manojmj92/subtitle_downloader)'}
+    url = "http://api.thesubdb.com/?action=download&hash={}&language=en".format(file_hash)
     if PY_VERSION == 3:
         req = urllib.request.Request(url, None, headers)
         response = urllib.request.urlopen(req).read()
@@ -43,10 +47,16 @@ def get_subtitles(file_hash):
         response = urllib2.urlopen(req).read()
     return response
 
+
 def is_movie_file_extension(extension):
-    return extension in [".avi", ".mp4", ".mkv", ".mpg", ".mpeg", ".mov", ".rm", ".vob", ".wmv", ".flv", ".3gp"]
+    '''Returns true if the passed in extension is a known movie extension.'''
+    return extension in [".avi", ".mp4", ".mkv", ".mpg", ".mpeg", ".mov",
+                         ".rm", ".vob", ".wmv", ".flv", ".3gp"]
+
 
 def sub_download(file_path):
+    '''Will try to download the subtitles for the given movie file path.
+    The subtitle file will be placed in the same folder as the movie file'''
     root, extension = os.path.splitext(file_path)
 
     # Skip this file if it is not a video
@@ -62,29 +72,32 @@ def sub_download(file_path):
         file_hash = get_hash(file_path)
         subs = get_subtitles(file_hash)
     except:
-        #Ignore exception and continue
-        print("Error in fetching subtitle for " + file_path)
+        # Ignore exception and continue
+        print("Error in fetching subtitle for {}".format(file_path))
         print("Error", sys.exc_info())
         return
 
-    print("Subtitle successfully Downloaded for file " + file_path)
+    print("Subtitle successfully Downloaded for {}".format(file_path))
     with open(root + ".srt", "wb") as subtitle:
         subtitle.write(subs)
 
+
 def sub_download_dir(path):
+    '''Recursively downloads subtitles of movie files in the given directory'''
     for dir_path, _, file_names in os.walk(path):
         for filename in file_names:
             file_path = os.path.join(dir_path, filename)
             sub_download(file_path)
 
+
 def main():
+    '''Main entry point'''
     if len(sys.argv) == 1:
         print("This program requires at least one parameter")
         sys.exit(1)
 
     for path in sys.argv:
         if os.path.isdir(path):
-            # Iterate the root directory recursively using os.walk and for each video file present get the subtitle
             sub_download_dir(path)
         else:
             sub_download(path)
--- test_sub_download.py
+++ test_sub_download.py
@@ -28,24 +28,26 @@ class TestSubDownloader(unittest.TestCase):
     def test_get_hash(self):
         '''Positive test for method get_hash.'''
         def float_to_str(number):
-          return "%.14f" % number
+            '''Converts a float to a string with 14 decimals'''
+            return "%.14f" % number
 
+        # Create a temp file with random repeatable data
         random.seed(1337)
         file_descriptor, tmp_path = tempfile.mkstemp()
         os.close(file_descriptor)
         with open(tmp_path, 'wb') as tmp_file:
             rand_data = [random.random() for _ in range(10000)]
             plaintext = " ".join(map(float_to_str, rand_data))
-            if MAJOR_VERSION == 2:
-                tmp_file.write(plaintext)
-            elif MAJOR_VERSION == 3:
-                tmp_file.write(bytes(plaintext, 'ascii'))
+            tmp_file.write(bytes(plaintext, 'ascii'))
+
         hashed = subdl.get_hash(tmp_path)
         os.remove(tmp_path)
         rand_str = float_to_str(rand_data[0])
         self.assertEqual("0.61775285695147", rand_str, "Random generator is not trustworthy.")
-        self.assertEqual("0.61775285695147 0.53326557360500 0.36584835924938", plaintext[:50], "float to str conversion likely different")
-        self.assertEqual("4f52910b28daeb6564c23f27d853377b", hashed, "Unexpected hash calculation returned")
+        self.assertEqual("0.61775285695147 0.53326557360500 0.36584835924938", plaintext[:50],
+                         "float to str conversion likely different")
+        self.assertEqual("4f52910b28daeb6564c23f27d853377b", hashed,
+                         "Unexpected hash calculation returned")
 
     def test_is_movie_extension(self):
         '''Test the is_movie_file_extention function.'''
